267a1b9c1c0efa021c7c2181198edd78cdd0fd76
Mohit kumar <async.mohitt@gmail.com>
2025-01-31 00:32:57 +0530

    Data has Been Injected

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
+import { getLocalStorage, setLocalStorage } from './utils/localStorage'
 
 const App = () => {
   return (
