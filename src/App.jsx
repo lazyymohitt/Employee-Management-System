564d570cb2d92abecff294412af6e1ba0cde9399
Mohit kumar <async.mohitt@gmail.com>
2025-02-05 09:53:37 +0530

    Add user state management to App component & Some Minor Fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
 import { getLocalStorage, setLocalStorage } from './utils/localStorage'
 
 const App = () => {
+  const [user, setUser] = useState('null');
+
   return (
     <>
-      <Login/>
-      {/* <EmployeeDashboard/> */}
+    {!user ? <Login/> : <h1>Hello bitch</h1> }
+      {/* <EmployeeDashboard/> */} 
       {/* <AdminDashboard/> */}
     </>
   )
