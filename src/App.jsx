72c97736c05f4ecb0aa7df1c9fc8cddcf49c4fba
Mohit kumar <async.mohitt@gmail.com>
2025-02-05 09:53:59 +0530

    Refactor user state initialization and implement login handling with credential validation

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,24 @@ import AdminDashboard from './components/Dashboard/AdminDashboard'
 import { getLocalStorage, setLocalStorage } from './utils/localStorage'
 
 const App = () => {
-  const [user, setUser] = useState('null');
+  const [user, setUser] = useState(null);
+
+  const handleLogin =(email,password )=>{
+    if(email === "admin@me.com" && password === 123) {
+      console.log("Admin has been logged ")
+    }
+    else if (email === "user@me.com" && password === 123) {
+       console.log( "User has been Arrived")
+    }
+    else{
+      alert("Invalid Credentials")
+    }
+  }
+  handleLogin("user@me.com",123)
 
   return (
     <>
-    {!user ? <Login/> : <h1>Hello bitch</h1> }
+    {!user ? <Login/> : <h1>User is Logged IN</h1> }
       {/* <EmployeeDashboard/> */} 
       {/* <AdminDashboard/> */}
     </>
