177b2a34a7384b1a9e0bfab7609ff82dcbb66979
Mohit kumar <async.mohitt@gmail.com>
2025-02-17 02:41:08 +0530

    Enhance CreateTask component with state management for task creation; refactor Header component for improved logout functionality and UI consistency

--- src/components/others/CreateTask.jsx
+++ src/components/others/CreateTask.jsx
@@ -1,28 +1,101 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { AuthContext } from '../../context/AuthProvider'
 
 const CreateTask = () => {
-  return (
-    <div className='bg-[#1C1C1C] rounded-md mt-2'>
-      <form className='flex px-4 py-2 justify-between'>
-        <div className='w-1/2 '>
-          <h3 className='text-sm'>Task Title </h3>
-          <input className='outline-none w-5/6 bg-transparent border-[1px] text-xs rounded-sm px-2 py-1 border-gray-400' type="text" placeholder='Make a UI design'/>
-          <h3 className='mt-1 text-sm'>Date</h3>
-          <input className='outline-none w-5/6 bg-transparent border-[1px] text-xs rounded-sm px-2 py-1 border-gray-400' type="date" name="" id="" />
-          <h3 className='mt-1 text-sm'>Assign To</h3>
-          <input className='outline-none w-5/6 bg-transparent border-[1px] text-xs rounded-sm px-2 py-1 border-gray-400' type="text" placeholder='Employee Name'/>
-          <h3 className='mt-1 text-sm'>Category</h3>
-          <input className='outline-none w-5/6 bg-transparent border-[1px] text-xs rounded-sm px-2 py-1 border-gray-400' type="text" placeholder='design,dev,etc' />
-        </div>
-        <div className='w-2/5 flex flex-col items-start'>
-                    <h3 className='text-sm text-gray-300 mb-0.5'>Description</h3>
-                    <textarea 
-                       className='w-full h-36 text-sm py-2 px-4 rounded outline-none bg-transparent border-[1px] border-gray-400' name="" id=""></textarea>
-                    <button className='bg-emerald-500 py-1 hover:bg-emerald-600 px-5 rounded text-sm mt-2 w-full'>Create Task</button>
+
+    const [userData, setUserData] = useContext(AuthContext)
+
+    const [taskTitle, setTaskTitle] = useState('')
+    const [taskDescription, setTaskDescription] = useState('')
+    const [taskDate, setTaskDate] = useState('')
+    const [asignTo, setAsignTo] = useState('')
+    const [category, setCategory] = useState('')
+
+    const [newTask, setNewTask] = useState({})
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+
+        setNewTask({ taskTitle, taskDescription, taskDate, category, active: false, newTask: true, failed: false, completed: false })
+
+        const data = userData
+
+        data.forEach(function (elem) {
+            if (asignTo == elem.firstName) {
+                elem.tasks.push(newTask)
+                elem.taskCounts.newTask = elem.taskCounts.newTask + 1
+            }
+        })
+        setUserData(data)
+        console.log(data);
+
+        setTaskTitle('')
+        setCategory('')
+        setAsignTo('')
+        setTaskDate('')
+        setTaskDescription('')
+
+    }
+
+    return (
+        <div className='p-5 bg-[#1c1c1c] mt-5 rounded'>
+            <form onSubmit={(e) => {
+                submitHandler(e)
+            }}
+                className='flex flex-wrap w-full items-start justify-between'
+            >
+                <div className='w-1/2'>
+                    <div>
+                        <h3 className='text-sm text-gray-300 mb-0.5'>Task Title</h3>
+                        <input
+                            value={taskTitle}
+                            onChange={(e) => {
+                                setTaskTitle(e.target.value)
+                            }}
+                            className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder='Make a UI design'
+                        />
+                    </div>
+                    <div>
+                        <h3 className='text-sm text-gray-300 mb-0.5'>Date</h3>
+                        <input
+                            value={taskDate}
+                            onChange={(e) => {
+                                setTaskDate(e.target.value)
+                            }}
+                            className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="date" />
+                    </div>
+                    <div>
+                        <h3 className='text-sm text-gray-300 mb-0.5'>Asign to</h3>
+                        <input
+                            value={asignTo}
+                            onChange={(e) => {
+                                setAsignTo(e.target.value)
+                            }}
+                            className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder='employee name' />
+                    </div>
+                    <div>
+                        <h3 className='text-sm text-gray-300 mb-0.5'>Category</h3>
+                        <input
+                            value={category}
+                            onChange={(e) => {
+                                setCategory(e.target.value)
+                            }}
+                            className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder='design, dev, etc' />
+                    </div>
                 </div>
-      </form>
-    </div>
-  )
+
+                <div className='w-2/5 flex flex-col items-start'>
+                    <h3 className='text-sm text-gray-300 mb-0.5'>Description</h3>
+                    <textarea value={taskDescription}
+                        onChange={(e) => {
+                            setTaskDescription(e.target.value)
+                        }} className='w-full h-44 text-sm py-2 px-4 rounded outline-none bg-transparent border-[1px] border-gray-400' name="" id=""></textarea>
+                    <button className='bg-emerald-500 py-3 hover:bg-emerald-600 px-5 rounded text-sm mt-4 w-full'>Create Task</button>
+                </div>
+
+            </form>
+        </div>
+    )
 }
 
 export default CreateTask
--- src/components/others/Header.jsx
+++ src/components/others/Header.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+import { setLocalStorage } from "../../utils/localStorage";
 
-const Header = ({data}) => {
+const Header = (props) => {
+  const logOutUser = () => {
+    localStorage.setItem("loggedInUser", "");
+    props.changeUser("");
+  };
 
-  const logOutUser = ()=>{
-    localStorage.setItem('loggedInUser','')
-      window.location.reload()
-  }
   return (
-    <div className=' flex items-center justify-between '>
-        <h1 className='text-xl font-medium'>Hello <br /> <span className='text-2xl font-semibold'>UserName👋 </span> </h1>
-        <button onClick={logOutUser} className='bg-red-600 px-2 py-1 rounded-sm text-xs'>Log Out</button>
+    <div className="flex items-end justify-between">
+      <h1 className="text-2xl font-medium">
+        Hello <br /> <span className="text-3xl font-semibold">username 👋</span>
+      </h1>
+      <button
+        onClick={logOutUser}
+        className="bg-red-600 text-base font-medium text-white px-5 py-2 rounded-sm"
+      >
+        Log Out
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
